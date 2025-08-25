1840ff2dcd3a6907fb0de62025f6309dd13711ec
yitzhak <isaac.aguilard@alumno.buap.mx>
2025-08-25 07:35:28 -0600

    add exponential

---
+++ expo.py
@@ -0,0 +1,26 @@
+import random
+import matplotlib.pyplot as plt
+import math
+
+lmbda = 0.5
+n = 100
+
+x_n = [-math.log(1 - random.uniform(0,1)) / lmbda for _ in range(n)]
+
+umbral = 1.0  # aca ponle el valor que quieras
+soles = 0
+aguilas = 0
+
+for numero in x_n:
+    if numero < umbral:
+        aguilas += 1
+    else:
+        soles += 1
+
+print("total de soles =", soles)
+print("total de aguilas =", aguilas)
+
+par = [soles, aguilas]
+fig, ax = plt.subplots()
+ax.bar(x=range(len(par)), height=par)
+plt.show()
