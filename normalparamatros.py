a241bc63de817efbfe3f6c6f9f0c2d6249bfbc3f
yitzhak <isaac.aguilard@alumno.buap.mx>
2025-09-01 10:10:40 -0600

    add gebbs html

--- normalparamatros.py
+++ normalparamatros.py
@@ -3,19 +3,22 @@ import random
 import math
 import matplotlib.pyplot as plt
 
-def normalEstandar(n):
+def normalEstandar(n,miu,sigma):
     u1=[random.uniform(0,1) for x in range(n)]
     u2=[random.uniform(0,1) for x in range(n)]
     x=[]
     for i in range(n):
         aux = math.sqrt(2*(math.log(1/u1[i])))*math.cos(6.283*u2[i])
-        x.append(aux)
+        aux2=sigma*aux+miu  
+        x.append(aux2)
 
     return x
 if __name__ == "__main__":
     #n es el numero de repeticiones de esta simulacion
-    n=1000000
-    valores = normalEstandar(n)
+    n=10000
+    miu=30
+    sigma=5
+    valores = normalEstandar(n,miu,sigma)
 
     fig, ax = plt.subplots()
     ax.hist(valores, bins=50, edgecolor="black", density=True)
